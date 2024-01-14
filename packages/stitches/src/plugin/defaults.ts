fbf07ac653104a5835f4fc7daf01b31a136c6673
kijv <93486795+kijv@users.noreply.github.com>
2024-01-14 14:24:58 -0500

    fix: biome

--- packages/stitches/src/plugin/defaults.ts
+++ packages/stitches/src/plugin/defaults.ts
@@ -1,10 +1,6 @@
 // picomatch patterns, used with rollup's createFilter
 export const defaultPipelineExclude = [];
-export const defaultPipelineInclude = [
-  /\.(vue|svelte|[jt]sx|astro)($|\?)/,
-];
+export const defaultPipelineInclude = [/\.(vue|svelte|[jt]sx|astro)($|\?)/];
 
 // micromatch patterns, used in postcss plugin
-export const defaultFilesystemGlobs = [
-  '**/*.{js,ts,jsx,tsx,vue,svelte,astro}',
-];
+export const defaultFilesystemGlobs = ['**/*.{js,ts,jsx,tsx,vue,svelte,astro}'];
